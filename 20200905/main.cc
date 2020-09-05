edbde44949c916860ccb8067a6d835f26d93ec9a
hidexir <hidexir@gmail.com>
2020-09-05 18:47:51 +0900

    WIP

---
+++ 20200905/main.cc
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <vector>
+using std::vector;
+using namespace std;
+
+class Solution {
+  public:             // Access specifier
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      vector<vector<int>> res;
+      return res;
+    }
+};
+
+int main() {
+  Solution solution;  // Create an object of MyClass
+  cout << solution.threeSum() << "\n";
+  return 0;
+}
